7e6e15ca9a18a7bfb232349bea21a62173b044c9
Martin Piazzon <miazzon@hotmail.com>
2021-02-14 06:32:52 -0300

    .

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,46 @@
 <template>
   <div>
-    <h1>test</h1>
+    <div class="p-grid p-mb-6">
+      <div class="p-col">
+        <Button label="Submit" icon="pi pi-check" iconPos="right" />
+        <Button label="Submit" class="p-button-text" />
+        <Button icon="pi pi-check" class="p-button-text" />
+        <Button label="Cancel" icon="pi pi-times" class="p-button-text" />
+        <Button
+          label="Search"
+          icon="pi pi-search"
+          iconPos="right"
+          class="p-button-text p-button-text"
+        />
+        <Button label="Primary" class="p-button-raised p-button-rounded" />
+        <Button label="Primary" class="p-button-outlined" />
+        <Button label="Link" class="p-button-link" />
+        <span class="p-buttonset">
+          <Button label="Save" icon="pi pi-check" />
+          <Button label="Delete" icon="pi pi-trash" />
+          <Button label="Cancel" icon="pi pi-times" />
+        </span>
+      </div>
+      <div class="p-col">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, atque sed
+        aut ab ipsam consequuntur labore et eos quo suscipit possimus odit quas,
+        molestias sapiente voluptatibus recusandae. Dignissimos, possimus aut.
+      </div>
+      <div class="p-col">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur minima
+        ex modi error nobis officia incidunt possimus, velit explicabo illum
+        unde, quae laudantium, soluta aspernatur laboriosam! Iusto voluptatibus
+        odio provident.
+      </div>
+    </div>
+
+    <hr />
     {{ count }}
-    <button @click="increment()">incrementar</button>
+    <div class="p-mt-6">
+      <Button @click="increment()">incrementar</Button>
+
+      <Button label="Submit" />
+    </div>
   </div>
 </template>
 
@@ -12,9 +50,10 @@ import { toRefs, getCurrentInstance } from "vue";
 export default {
   name: "App",
   setup() {
+    const display = true;
     const { state, increment } = global;
     increment();
-    return { ...toRefs(state), increment };
+    return { ...toRefs(state), increment, display };
   },
 };
 </script>
--- app/src/main.js
+++ app/src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+
+import "primevue/resources/themes/md-light-indigo/theme.css"; //theme
+import "primevue/resources/primevue.min.css"; //core css
+import "primeicons/primeicons.css"; //icons
+import "primeflex/primeflex.css";
+
 import "./index.css";
 
-createApp(App).mount("#app");
+import PrimeVue from "primevue/config";
+import Dialog from "primevue/dialog";
+
+import Button from "primevue/button";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component("Dialog", Dialog);
+app.component("Button", Button);
+
+app.mount("#app");
