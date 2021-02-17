520543afa95b5d11294d494fda24c0f90810684b
Martin Piazzon <mpiazzon@hotmail.com>
2021-02-17 20:38:17 -0300

    .

---
+++ app-tailwind/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div class="box">
+      <h2>Probando esto</h2>
+      <p class="bg-red-200 text-2xl text-blue-200">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur
+        laudantium, delectus, suscipit non fugiat ducimus ad officiis,
+        reprehenderit repellendus repudiandae eveniet officia in tempore quos
+        libero amet! Amet, tempora porro!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import global from "./composables/global.js";
+import { toRefs, getCurrentInstance } from "vue";
+export default {
+  name: "App",
+  setup() {
+    const display = true;
+  },
+};
+</script>
---
+++ app-tailwind/tailwind.config.js
@@ -0,0 +1,21 @@
+const colors = require("tailwindcss/colors");
+module.exports = {
+  purge: {
+    mode: "layers",
+    content: ["./public/**/*.html", "./src/**/*.vue"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        emerald: colors.emerald,
+        gray: colors.trueGray,
+        red: colors.rose,
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
