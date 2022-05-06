62c13b9d14784619f223175e1873c079618ced43
Martin Piazzon <mpiazzon@hotmail.com>
2022-05-05 22:51:45 -0400

    .

---
+++ app-svelte/src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
---
+++ app-svelte/src/routes/index.svelte
@@ -0,0 +1,5 @@
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+<section />
